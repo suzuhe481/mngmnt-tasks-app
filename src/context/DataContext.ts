e4800dd07d7862c03bdf911e4950058c046694ed
Hector <baseballhector@gmail.com>
2025-02-17 20:14:10 -0500

    New values for context for sorting settings

--- src/context/DataContext.ts
+++ src/context/DataContext.ts
@@ -1,13 +1,16 @@
 import { createContext } from "react";
 
 // Types
-import { ITask } from "../types/types";
+import { ITask, ISortedFilteredSettings } from "../types/types";
 
 interface IDataContextProps {
   tasksData: ITask[];
+  sortedFilteredData: ITask[] | [];
   addTask: (newTask: ITask) => void;
   editTask: (editedTask: ITask) => void;
   deleteTask: (deleteTask: ITask) => void;
+  sortedOrFilteredSettings: ISortedFilteredSettings;
+  changeColumnSorted: (newColumn: string) => void;
 }
 
 export const DataContext = createContext<IDataContextProps | undefined>(
