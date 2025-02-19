1fef5dd6838d7ab27d3ae6ab4823fb6e89ecb243
Hector <baseballhector@gmail.com>
2025-02-18 21:32:33 -0500

    Displays Notification Modal based on state

--- src/UI/SettingsModals/ActionModal.tsx
+++ src/UI/SettingsModals/ActionModal.tsx
@@ -1,6 +1,8 @@
 import { RefObject, use, useState } from "react";
 import { DataContext } from "../../context/DataContext";
 
+import NotificationModal from "../NotificationModal/NotificationModal";
+
 interface IFilterModalProps {
   ref: RefObject<HTMLDivElement | null>;
 }
@@ -44,10 +46,24 @@ const ActionModal = ({ ref }: IFilterModalProps) => {
       ref={ref}
       className="absolute flex flex-col w-[200px] z-20 rounded-xl overflow-hidden bg-slate-100 border-1 border-slate-400 right-0 top-full mt-1 shadow-lg"
     >
+      {importModalOpen ? (
+        <NotificationModal
+          title={ImportDataTitle}
+          description={ImportDataDesc}
+          confirmAction={confirmImport}
+          cancelAction={closeImportModalModal}
+        />
+      ) : null}
       <button
         className={`font-bold cursor-pointer border-b-1 border-slate-400 py-4 text-slate-600  bg-slate-100 hover:bg-blue-200`}
       >
         Reset Filters
+      </button>
+      <button
+        onClick={openImportModalModal}
+        className={`font-bold cursor-pointer border-b-1 border-slate-400 py-4 text-slate-600 last:border-0 bg-slate-100 hover:bg-blue-200`}
+      >
+        Load Example Data
       </button>
     </div>
   );
