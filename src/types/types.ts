c8b653caba0c71ca8c30b84b283ad349390fe35e
Hector <baseballhector@gmail.com>
2025-02-20 20:28:12 -0500

    Adding customFields to ITask

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,8 +1,11 @@
+// Record is used so now the title of a field is a key in
+// the object, and it's value is ICustomData
 export interface ITask {
   id?: number;
   title: string;
   status: string;
   priority: string;
+  customFields?: Record<string, ICustomData>;
 }
 
 export interface ISortedFilteredSettings {
