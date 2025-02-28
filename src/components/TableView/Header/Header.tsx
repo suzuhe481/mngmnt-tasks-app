924a67536d6a24c177d486d35c8ce5964cd14b8d
Hector <baseballhector@gmail.com>
2025-02-27 20:50:08 -0500

    Changed text size of toggle button

--- src/components/TableView/Header/Header.tsx
+++ src/components/TableView/Header/Header.tsx
@@ -16,7 +16,7 @@ export const Header = () => {
     <div className="relative flex flex-row justify-center py-4 bg-slate-700">
       <button
         onClick={toggleKanbanView}
-        className="animate-all absolute left-2 text-white cursor-pointer border-1 border-white p-2 rounded-md text-[0.6rem] lg:text-base top-1/2 -translate-y-1/2 hover:bg-slate-300 hover:text-slate-800"
+        className="animate-all absolute left-2 text-white cursor-pointer border-1 border-white p-2 rounded-md text-[0.8rem] lg:text-base top-1/2 -translate-y-1/2 hover:bg-slate-300 hover:text-slate-800"
       >
         Switch to {kanbanView ? "Table" : "Kanban"}
       </button>
