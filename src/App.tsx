30d37628975477b7bdb207e58c6b5c7bde4697bc
Hector <baseballhector@gmail.com>
2025-02-27 21:04:52 -0500

    Using the kanbanView variable to display TableView or KanbanView

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
+import { use } from "react";
+
 import { TableView } from "./components/TableView/TableView";
+import { KanbanView } from "./components/KanbanView/KanbanView";
+
+import { DataContext } from "./context/DataContext";
 
 function App() {
-  return <TableView />;
+  const context = use(DataContext);
+
+  // Checks for undefined context.
+  if (!context) {
+    return;
+  }
+
+  const { kanbanView } = context;
+
+  return <>{kanbanView ? <KanbanView /> : <TableView />};</>;
 }
 
 export default App;
