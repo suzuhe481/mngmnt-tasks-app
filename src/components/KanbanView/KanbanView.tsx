b7021e5382c0e39826ccc154942e0e7df704c3ab
Hector <baseballhector@gmail.com>
2025-02-27 21:04:04 -0500

    Initial kanban page view component

--- src/components/KanbanView/KanbanView.tsx
+++ src/components/KanbanView/KanbanView.tsx
@@ -1,3 +1,14 @@
+import { Header } from "../TableView/Header/Header";
+import { Search } from "../TableView/Search/Search";
+import { KanbanTable } from "./KanbanTable/KanbanTable";
+// import { KanbanTable } from "./KanbanTable/KanbanTable";
+
 export const KanbanView = () => {
-  return <div>KanbanView</div>;
+  return (
+    <div className="w-full">
+      <Header />
+      <Search />
+      <KanbanTable />
+    </div>
+  );
 };
